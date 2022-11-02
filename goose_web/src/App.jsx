e6850ec7650ccfb8845d58a7791d4670d0aa75de
Sophia Sun <53122458+sophiiasun@users.noreply.github.com>
2022-11-02 15:27:34 -0400

    Updated App file

--- goose_web/src/App.jsx
+++ goose_web/src/App.jsx
@@ -7,22 +7,12 @@ import './App.css'
 
 function App() {
   return (
-<<<<<<< Updated upstream
-    <div style={{height:'80vh', width:'80vw', paddingTop:'30vh'}} className="App" onClick={() => alert("hi")}>
-      <div>
-        <h1>Welcome to SecureIT.</h1>
-        <h2>Click anywhere to begin.</h2>
-      </div>
-    </div>
-  )
-=======
        <Router>
         <Routes>
           <Route path = '/' element={<Home/>}/>
         </Routes>
       </Router>
   );
->>>>>>> Stashed changes
 }
 
 
