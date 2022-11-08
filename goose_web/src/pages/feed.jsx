60f8e46a9d25b83002d4c828cfdf9f13d2ce0184
Thomas <thomaswang003@gmail.com>
2022-11-08 16:38:37 -0500

    Website CSS Changes

--- goose_web/src/pages/feed.jsx
+++ goose_web/src/pages/feed.jsx
@@ -5,13 +5,13 @@ import { useNavigate } from 'react-router-dom';
 
 const Feed = () => {
     return (
-        <>
+        <div className = "feedContainer fadeIn">
             <div>
                 <h1> Here are your feeds </h1> 
                 
             </div>
             <Cards title = "Front Door" feedLink = "https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1"/>   
-        </>
+        </div>
     )
 }
   
