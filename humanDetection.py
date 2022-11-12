3b9bb1b4f0d26edb0aebde37e3b00cf9cbc499c5
Matteo Pesenti <40373353+matteo-psnt@users.noreply.github.com>
2022-11-11 21:35:48 -0500

    Included reference

--- humanDetection.py
+++ humanDetection.py
@@ -5,6 +5,7 @@ import cv2
 def human_detected(frame):
     # initialize the HOG descriptor/person detector
     hog = cv2.HOGDescriptor()
+    # Model from [OpenCV Documentation](https://docs.opencv.org/4.x/d5/d33/structcv_1_1HOGDescriptor.html)
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
     # resizing for faster detection
@@ -12,7 +13,8 @@ def human_detected(frame):
 
     # returns the bounding boxes for the detected objects
     boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
-
+    
+    # returns true if it detects more than one human
     if len(boxes) >= 1:
         return True
     else:
