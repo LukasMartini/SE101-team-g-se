e4b487d3ba2c479301d4315bcdf71dedcecc0626
Matteo_psnt <matteopesenti229@gmail.com>
2022-11-29 18:47:25 -0500

    l bozo

--- playSound.py
+++ playSound.py
@@ -1,5 +1,16 @@
-# import required module
-from playsound import playsound
-
-# for playing doorbell.wav file
-playsound('doorbell.wav')
+# credit: https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/ 
+# import required module 
+from playsound import playsound 
+import RPi.GPIO as GPIO 
+# Import Raspberry Pi GPIO library 
+GPIO.setwarnings(False) 
+# Ignore warning for now 
+GPIO.setmode(GPIO.BOARD) 
+# Use physical pin numbering 
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+# Set pin 10 to be an input pin and set initial value to be pulled low (off) 
+while True: 
+# Run forever 
+    if GPIO.input(10) == GPIO.HIGH: 
+        print("Button was pushed!") # for playing doorbell.wav file 
+        playsound('doorbell.wav')
