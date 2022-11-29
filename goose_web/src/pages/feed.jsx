8ba75903958e45a0e81577575fdf31045d3e9134
alakhpc <36898190+alakhpc@users.noreply.github.com>
2022-11-29 17:26:49 -0500

    url new

--- goose_web/src/pages/feed.jsx
+++ goose_web/src/pages/feed.jsx
@@ -10,7 +10,7 @@ const Feed = () => {
                 <h1> Here are your feeds </h1> 
                 
             </div>
-            <Cards title = "Front Door" feedLink = "http://127.0.0.1:8080"/>   
+            <Cards title = "Front Door" feedLink = "http://se101.alakhpc.com:8080"/>   
         </div>
     )
 }
