113aba51ece70776ebc9a232603ffb425b6f6bc5
alakhpc <36898190+alakhpc@users.noreply.github.com>
2022-12-01 14:44:40 -0500

    not completed mci

--- goose_web/src/components/cards.jsx
+++ goose_web/src/components/cards.jsx
@@ -1,30 +1,65 @@
-import {React, useState} from 'react'
-import { useNavigate, } from 'react-router-dom';
-import './cards.css'
+import { ReactMic } from "react-mic";
+import { React, useState } from "react";
+import "./cards.css";
 // credits: sophia
 
 function Popup(props) {
-	return (props.trigger) ? (
-		<div className='popup'> 
-			<div className='popup-inner'>
-				<button className='close-btn' onClick={() => props.setTrigger(false)}>x</button>
-				<iframe src = {props.children} style = {{height: "100%", width: "100%"}}/>
-			</div>
-		</div>
-	) : "";
+  const [isRecording, setRecording] = useState(false);
+
+  return props.trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <ReactMic
+          record={isRecording}
+          className="micnone"
+          onStop={(blob) => {}}
+          onData={(blob) => {
+            let fd = new FormData();
+            fd.append("blob", blob, "v.webm");
+            fetch("http://localhost:8080/mic", {
+              method: "POST",
+              body: fd,
+            });
+          }}
+        />
+        {/* <button onClick={() => setRecording(true)}>start audio</button> */}
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          x
+        </button>
+        <iframe
+          src={props.children}
+          style={{ height: "100%", width: "100%" }}
+        />
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
 
-function cards({title, img, feedLink}) {
-    const [showOverlay, setOverlay]  = useState(false); 
-    return (
-        <>
-            <Popup trigger={showOverlay} setTrigger={setOverlay} children = {feedLink}></Popup>
-            <div className = "cardContainer" onClick={()=> setOverlay(true)}>
-                <h2 style = {{ fontWeight: '600' }}> {title} </h2>
-                <img src = "./logo.png" style = {{objectFit: 'contain', width: '5rem', height: '5rem', filter: 'invert(100%)'}}/> 
-            </div> 
-        </>
-    )
+function cards({ title, img, feedLink }) {
+  const [showOverlay, setOverlay] = useState(false);
+  return (
+    <>
+      <Popup
+        trigger={showOverlay}
+        setTrigger={setOverlay}
+        children={feedLink}
+      ></Popup>
+      <div className="cardContainer" onClick={() => setOverlay(true)}>
+        <h2 style={{ fontWeight: "600" }}> {title} </h2>
+        <img
+          src="./logo.png"
+          style={{
+            objectFit: "contain",
+            width: "5rem",
+            height: "5rem",
+            filter: "invert(100%)",
+          }}
+        />
+      </div>
+    </>
+  );
 }
 
-export default cards
+export default cards;
--- goose_web/src/pages/feed.jsx
+++ goose_web/src/pages/feed.jsx
@@ -1,42 +1,59 @@
-import './pages.css'
-import {React, useState} from 'react'
-import Cards from '../components/cards'
+import "./pages.css";
+import { React, useState } from "react";
+import Cards from "../components/cards";
 
-const loginPage = () => {     
-    const [show, setShow] = useState(false); 
-    const pop_up = () => {
-        document.getElementById("accounts").className = 'fade';
-        setTimeout(() => {setShow(true)}, 500);  
-    }
+const loginPage = () => {
+  const [show, setShow] = useState(false);
+  const pop_up = () => {
+    document.getElementById("accounts").className = "fade";
+    setTimeout(() => {
+      setShow(true);
+    }, 500);
+  };
   return (
     <>
-        {show && <Feed/> }
-        {!show && <Login/>}
+      {show && <Feed />}
+      {!show && <Login />}
     </>
-  )
-    function Login() {
-        return (
-            <div id = "accounts">
-                <form action = "" method = "get" >
-                    <h2 style = {{fontSize: "1.6rem"}}>Sign in to secureIT</h2>
-                    <div style = {{ display: "flex", flexDirection: "column"}}>
-                        <input type = "text" className = "inputBox" placeholder="username" style = {{zIndex: "1"}}></input>
-                        <input type = "text" className = "inputBox" placeholder = "password" ></input>
-                    </div>
-                    <button type = "button" onClick={() => pop_up()} style = {{ margin: "1rem"}}>Enter</button>
-                </form>
-            </div>
-        )
-    }
-}
+  );
+  function Login() {
+    return (
+      <div id="accounts">
+        <form action="" method="get">
+          <h2 style={{ fontSize: "1.6rem" }}>Sign in to secureIT</h2>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <input
+              type="text"
+              className="inputBox"
+              placeholder="username"
+              style={{ zIndex: "1" }}
+            ></input>
+            <input
+              type="text"
+              className="inputBox"
+              placeholder="password"
+            ></input>
+          </div>
+          <button
+            type="button"
+            onClick={() => pop_up()}
+            style={{ margin: "1rem" }}
+          >
+            Enter
+          </button>
+        </form>
+      </div>
+    );
+  }
+};
 
 function Feed() {
-    return (
-        <div id = "feed">
-            <h1> Here are your feeds </h1> 
-            <Cards title = "Front Door" feedLink = "http://127.0.0.1:8080"/>   
-        </div>
-    )
+  return (
+    <div id="feed">
+      <h1> Here are your feeds </h1>
+      <Cards title="Front Door" feedLink="http://se101.alakhpc.com:8080" />
+    </div>
+  );
 }
 
-export default loginPage
+export default loginPage;
