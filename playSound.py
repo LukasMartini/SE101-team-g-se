d07f0838a0d926f8ffc55f2b5c53b7f90a682f25
alakhpc <36898190+alakhpc@users.noreply.github.com>
2022-12-01 10:13:07 -0500

    fix

--- playSound.py
+++ playSound.py
@@ -1,25 +1,27 @@
 # credit: https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
 # import required module
 
-try:
 
-    import RPi.GPIO as GPIO
-    import simpleaudio as sa
+def playSound():
+    try:
 
-    wave_obj = sa.WaveObject.from_wave_file("doorbell.wav")
-    # Import Raspberry Pi GPIO library
-    GPIO.setwarnings(False)
-    # Ignore warning for now
-    GPIO.setmode(GPIO.BOARD)
-    # Use physical pin numbering
-    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-    while True:
-        # Run forever
-        if GPIO.input(10) == GPIO.HIGH:
-            print("Button was pushed!")  # for playing doorbell.wav file
-            play_obj = wave_obj.play()
-            play_obj.wait_done()
+        import RPi.GPIO as GPIO
+        import simpleaudio as sa
 
-except ImportError:
-    print("rip no button")
+        wave_obj = sa.WaveObject.from_wave_file("doorbell.wav")
+        # Import Raspberry Pi GPIO library
+        GPIO.setwarnings(False)
+        # Ignore warning for now
+        GPIO.setmode(GPIO.BOARD)
+        # Use physical pin numbering
+        GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+        while True:
+            # Run forever
+            if GPIO.input(10) == GPIO.HIGH:
+                print("Button was pushed!")  # for playing doorbell.wav file
+                play_obj = wave_obj.play()
+                play_obj.wait_done()
+
+    except ImportError:
+        print("rip no button")
