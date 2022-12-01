6192b7e155ffbd0140734815fbe61084dd853bbf
alakhpc <36898190+alakhpc@users.noreply.github.com>
2022-12-01 16:01:17 -0500

    notification

--- playSound.py
+++ playSound.py
@@ -4,6 +4,7 @@
 import simpleaudio as sa
 
 import limiter
+import notifications
 
 
 def playsound():
@@ -25,6 +26,10 @@ def playsound():
         # Run forever
         if GPIO.input(10) == GPIO.HIGH:
             print("Button was pushed!")  # for playing doorbell.wav file
+            notifications.send_notification(
+                "Someone is at the door!",
+                "check out the stream here\n http://se101.alakhpc.com:8080",
+            )
             ps(1)
 
 
